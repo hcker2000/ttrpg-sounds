268d8f43ca5ed3fa603517322f45d681de931b1a
Mike Ashcraft <mashcraft@mach2.org>
2024-09-29 20:33:28 -0400

    Added some basic example code on how to play sounds

--- src/renderer/src/scene/content.tsx
+++ src/renderer/src/scene/content.tsx
@@ -5,11 +5,28 @@ import { Sound } from "./sound";
 export function Content() {
     const { store } = useStore();
 
+    const playScene = (event) => {
+        // todo: place this in the add audio call back once I get that element done. it should save the file path.
+        const hiddenFileInput = document.getElementById('hiddenFileInput');
+        hiddenFileInput.click();
+      
+        hiddenFileInput.addEventListener('change', () => {
+          const file = hiddenFileInput.files[0];
+          const urlObj = URL.createObjectURL(file);
+          
+          const audioPlayer = new Audio(urlObj);
+          audioPlayer.play()
+        });
+
+        // todo: loop over all store.sounds and start them playing
+    }
+
     return (
         <Show when={store.selectedScene} fallback={<div>Loading...</div>}>
+            <input type="file" id="hiddenFileInput"></input>
             <div className="title">
                 <h3 class="">{store.selectedScene.title}</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                <svg on:click={playScene} xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-play-circle-fill pointer" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
                 </svg>
             </div>
