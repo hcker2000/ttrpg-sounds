72df24f712c40500815a5fca32dc3aad41f852ce
Mike Ashcraft <mashcraft@mach2.org>
2024-10-01 20:01:43 -0400

    Adjusted the scene provider so vscode knows whats what

--- src/renderer/src/scene/nav.tsx
+++ src/renderer/src/scene/nav.tsx
@@ -1,10 +1,11 @@
 
+import { For } from 'solid-js';
 import { unwrap } from "solid-js/store";
 import Swal from "sweetalert2"
 import { useStore } from "./provider";
 
 export function Nav() {
-    const { store } = useStore();
+    const { store, setStore, newScene, setSelectedScene } = useStore();
 
     const addScene = async (event) => {
         const { value: title } = await Swal.fire({
@@ -23,23 +24,19 @@ export function Nav() {
             let newSceneObject = structuredClone(unwrap(store.defaultScene))
     
             newSceneObject.title = title
-            store.newScene(newSceneObject)
+            newScene(newSceneObject)
         }
     }
-    
-    const changeScene = (scene, event) => {
-        store.setSelectedScene(scene)
-    }    
-      
+
     return (
         <div>
             <div class="d-grid gap-2">
-                <button type="button" class="btn btn-success btn-sm mb-3" id="addScene" on:click={addScene}>Add</button>
+                <button type="button" class="btn btn-success btn-sm mb-3" id="addScene" onClick={addScene}><i class="bi bi-plus-circle-fill"></i> Add Scene</button>
             </div>
             <ul class="nav flex-column nav-fill">
                 <For each={store.scenes}>{(scene, i) =>
                     <li class="nav-item">
-                        <a class="nav-link active" on:click={[changeScene, scene]} href="#" title={scene.description}>{scene.title}</a>
+                        <a class="nav-link active" onClick={[setSelectedScene, scene]} href="#" title={scene.description}>{scene.title}</a>
                     </li>
                 }</For>
             </ul>
--- src/renderer/src/scene/provider.tsx
+++ src/renderer/src/scene/provider.tsx
@@ -1,54 +1,50 @@
-import { createContext, useContext } from 'solid-js';
+import { createContextProvider } from '@solid-primitives/context';
 import { createStore, unwrap } from 'solid-js/store';
 
-// Create a context
-const StoreContext = createContext();
-
-// Define the provider component that will hold the store
-export function SceneProvider(props) {
-    // Create a store (could be any complex object or state)
-    const [store, setStore] = createStore({
-        defaultScene: {
-            title: "Hello World",
-            description: "A wizard is never late, Frodo Baggins. Nor is he early. He arrives precisely when he means to. 300 Athelas fulfilled selling. Hell profit Haleth rare! Garden reaction night ancestor prelude boys Sounds sweeps 3434. Usually titles king election foothold Thorin Oakenshield themselves stinks corrupted round fulfill. Silver unseen rampart fool Galadriel loosened feather vanquished treat named. Horn terribly reputation fellow's mercenaries crows Dwarvish worst sea convinced nothing's. Forebearers hasn't lead carcasses deputy overfond crevice maiden Gríma roaring ill-tempered is.",
-            sounds: [
-                {
-                    title: "birds",
-                    file: "/home/hcker2000/Downloads/file_example_MP3_700KB.mp3",
-                    volume: 25,
-                    loop: true,
-                    status: false,
-                    player: null
-                },
-                {
-                    title: "river",
-                    file: "/home/hcker2000/Downloads/sample-9s.mp3",
-                    volume: 0,
-                    loop: true,
-                    status: false,
-                    player: null
-                },
-            ],
-            tags: [
-                "tavern",
-                "night"
-            ]
-        },
-        selectedScene: null,
-        scenes: [],
-        newScene: (newScene) => setStore("scenes", store.scenes.length, newScene),
-        setSelectedScene: (scene) => setStore("selectedScene", structuredClone(unwrap(scene)))
-    })
-
-    // Pass the store to the context provider
-    return (
-        <StoreContext.Provider value={{store: store, setStore: setStore}}>
-            {props.children}
-        </StoreContext.Provider>
-    );
+const initialStoreValue = {
+    defaultScene: {
+        title: "Hello World",
+        description: "A wizard is never late, Frodo Baggins. Nor is he early. He arrives precisely when he means to. 300 Athelas fulfilled selling. Hell profit Haleth rare! Garden reaction night ancestor prelude boys Sounds sweeps 3434. Usually titles king election foothold Thorin Oakenshield themselves stinks corrupted round fulfill. Silver unseen rampart fool Galadriel loosened feather vanquished treat named. Horn terribly reputation fellow's mercenaries crows Dwarvish worst sea convinced nothing's. Forebearers hasn't lead carcasses deputy overfond crevice maiden Gríma roaring ill-tempered is.",
+        sounds: [
+            {
+                title: "birds",
+                file: "/home/hcker2000/Downloads/file_example_MP3_700KB.mp3",
+                volume: 25,
+                loop: true,
+                status: false,
+                player: null
+            },
+            {
+                title: "river",
+                file: "/home/hcker2000/Downloads/sample-9s.mp3",
+                volume: 0,
+                loop: true,
+                status: false,
+                player: null
+            },
+        ],
+        tags: [
+            "tavern",
+            "night"
+        ]
+    },
+    selectedScene: null,
+    scenes: []
 }
 
-// Export a hook for easy access to the store in any component
-export function useStore() {
-    return useContext(StoreContext);
+const createInitialStoreValue = () => {
+    return initialStoreValue;
 }
+
+const [store, setStore] = createStore(initialStoreValue)
+
+export const [SceneProvider, useStore] = createContextProvider(() => {
+    const [store, setStore] = createStore(createInitialStoreValue());
+    
+    return {
+      store: store,
+      setStore: setStore,
+      newScene: (newScene) => setStore("scenes", store.scenes.length, newScene),
+      setSelectedScene: (scene) => setStore("selectedScene", structuredClone(unwrap(scene)))
+    }
+});
